d7dd23bcb5c3aa59c8ccda19904063384c1626a9
dennenn <dennenn@gmail.com>
2014-05-27 00:32:23 +0900

    defined JS format at first

---
+++ js/jquery.retinaImageLoader.js
@@ -0,0 +1,13 @@
+(function ($) {
+
+  $.fn.retinaImageLoader = function (options) {
+    var settings = $.extend({
+      
+    }, options);
+
+    return this.each(function() {
+
+      
+    });
+  };
+})(jQuery);
